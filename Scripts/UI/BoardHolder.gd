1bb14df7e974b293c84c7a6072a6777eb250c2f3
Empika1 <dravidts1@gmail.com>
2024-03-27 22:03:07 -0400

    aspect ratioing

--- Scripts/UI/BoardHolder.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-class_name BoardHolder
-
-@export var board: Sprite2D
-
-func _process(_delta):
-	board.position = size * 0.5 - (board.get_rect().size * board.global_scale) * 0.5
-	var scaleRatio: float = (board.get_rect().size * board.global_scale).x / min(size.x, size.y)
-	board.global_scale /= Vector2(scaleRatio, scaleRatio)
