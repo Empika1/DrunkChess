c0c3d13f4a7939d1599ff1703e102eaa25ba5c0b
Empika1 <dravidts1@gmail.com>
2024-04-01 14:11:53 -0400

    how make ui

---
+++ Scripts/UI/LabelScaler.gd
@@ -0,0 +1,34 @@
+@tool
+
+extends Control
+
+enum FitType {
+	FIT_WIDTH,
+	FIT_HEIGHT,
+	FIT,
+	COVER
+}
+
+var initialHeight: float = 0.;
+var initialWidth: float = 0.;
+@export var fontSize: float
+@export var fitType: FitType
+@export var label: Label
+func _process(_delta: float):
+	if initialHeight == 0 and size.y != 0:
+		initialWidth = size.x
+		initialHeight = size.y
+	
+	var wantedSize: int = 0
+	match fitType:
+		FitType.FIT_WIDTH:
+			wantedSize = int(fontSize * size.x / initialWidth)
+		FitType.FIT_HEIGHT:
+			wantedSize = int(fontSize * size.y / initialHeight)
+		FitType.FIT:
+			wantedSize = min(int(fontSize * size.x / initialWidth), int(fontSize * size.y / initialHeight))
+		FitType.COVER:
+			wantedSize = max(int(fontSize * size.x / initialWidth), int(fontSize * size.y / initialHeight))
+	
+	if label.get_theme_font_size("font_size") != wantedSize:
+		label.add_theme_font_size_override("font_size", wantedSize)
