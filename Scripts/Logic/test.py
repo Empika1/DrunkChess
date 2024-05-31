2d2af75d1ef74347cc7dbdb127e8fa4b6db6318a
Empika1 <dravidts1@gmail.com>
2024-05-30 22:58:30 -0400

    clean up test files

--- Scripts/Logic/test.py
+++
@@ -1,3 +0,0 @@
-import math
-for i in range(64):
-    print(2**128 > math.factorial(64) / math.factorial(64-i), i)
--- Scripts/test.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _ready():
-	var b: BitArray = BitArray.new()
-	b.changeLength(64)
-	b.setFromFloat(0, 3000)
-	print(b.dataToString())
